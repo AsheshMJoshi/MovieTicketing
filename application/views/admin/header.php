172b92e25bf4fc3bc3e9e794e205036e271cf9aa
Ashesh Man Joshi <ashesh_joshi@hotmail.com>
2014-05-29 17:11:36 +0545

    Update

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -34,7 +34,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Movie</a>
+            <a class="navbar-brand" href="#"><img src="<?php echo base_url();?>assets/image/icon.png"/></a>
         </div>
         <div class="collapse navbar-collapse">
 
